eda7ac02b607403d298841c56dfca60351f5b31f
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-22 23:56:22 +0100

    Implemented the cross-origin in the AuthController file

--- src/main/java/net/sixtusdev/taskmanager/controller/auth/AuthController.java
+++ src/main/java/net/sixtusdev/taskmanager/controller/auth/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 
 public class AuthController {
 
